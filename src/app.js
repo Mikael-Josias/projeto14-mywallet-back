2168054117f941ddd5f4b1e038232a1946a0c79d
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-17 12:55:59 -0300

    feat: connected to mongodb database

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 import dayjs from "dayjs";
 import Joi from "joi";
 import chalk from "chalk";
@@ -12,6 +13,17 @@ const PORT = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+let mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+try {
+    await mongoClient.connect();
+    db = mongoClient.db();
+    console.log(chalk.blue.bgBlueBright("BANCO CONECTADO!")); 
+} catch (err) {
+    console.log(chalk.red.bgRed(`ERRO AO CONECTAR AO BANCO: ${err}`));
+}
 
 app.listen(PORT, () => {
     console.log(chalk.black.bgGreenBright("🚀!SERVER INICIALIZADO!🚀"));
